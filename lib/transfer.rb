cfff6f515aa37b0652966ebf51ca65efca956413
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-05 17:51:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,8 +28,8 @@ class Transfer
 
   def reverse_transfer
     if valid? && receiver.balance > amount && self.status == "pending"
-      self.sender.balance += amount
-      self.receiver.balance -= amount
+      sender.balance += amount
+      receiver.balance -= amount
       self.status = "reversed"
     else
       self.status = "rejected"
