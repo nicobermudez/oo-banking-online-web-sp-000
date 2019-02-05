7c31ee992a2f0566e889ddba528289041abe489d
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-05 17:28:50 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,12 +1,20 @@
 class BankAccount
-  attr_accessor :name, :balance, :status
-  attr_reader
+  attr_accessor :balance, :status
+  attr_reader :name
   attr_writer
 
   def initialize(name)
     @name=name
     @balance = 1000
     @status = "open"
+  end
+
+  def deposit
+    
+  end
+
+  def display_balance
+    self.balance
   end
 
 end
