62392836b7a8adf54081eaaaa776a2861a01b4cb
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-05 17:32:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,13 @@
 class Transfer
   # your code here
+  attr_accessor :balance, :status
+  attr_reader :name
+  attr_writer
+
+  def initialize(sender, receiver, amount)
+    @sender=sender
+    @receiver=receiver
+    @status = "pending"
+    @amount = amount
+  end
 end
